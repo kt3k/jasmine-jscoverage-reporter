2e6b202c362b3793f43f4633b48a73ad917122df
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-22 22:58:57 +0900

    Use constant for coverage label

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+var COVERAGE_LABEL = 'coverage:';
+
 window.jasmine.getEnv().addReporter({
     reportRunnerResults: function () {
         'use strict';
 
         if (window._$jscoverage != null) {
-            window.phantom.sendMessage('writeln', '\ncoverage:' + JSON.stringify(window._$jscoverage));
-        }   
-    }   
+            window.phantom.sendMessage('writeln', '\n' + COVERAGE_LABEL + JSON.stringify(window._$jscoverage));
+        }
+    }
 });
