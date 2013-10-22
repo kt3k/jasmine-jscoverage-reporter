cfbfb834ae0907f9efef87860e0918bae195afde
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-22 13:24:32 +0900

    Add reporter

---
+++ index.js
@@ -0,0 +1,9 @@
+window.jasmine.getEnv().addReporter({
+    reportRunnerResults: function () {
+        'use strict';
+
+        if (window._$jscoverage != null) {
+            window.phantom.sendMessage('writeln', '\ncoverage:' + JSON.stringify(window._$jscoverage));
+        }   
+    }   
+});
